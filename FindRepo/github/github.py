19ac36a3db655146ddd2b4fbbc80f39cd9f283d6
Кирилл Горюнов <kirillgorunov@MacBook-Pro-Kirill.local>
2024-01-30 23:57:47 +0300

    FIX: remove print

--- FindRepo/github/github.py
+++ FindRepo/github/github.py
@@ -9,7 +9,6 @@ from typing import List
 
 @functools.lru_cache(maxsize=10000)
 def find_github_code(code: str, count: int = 5) -> List[str]:
-    print('cahca')
     if count <= 0:
         raise RuntimeError('Count less than zero!')
 
