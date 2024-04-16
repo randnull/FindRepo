162370fb3fb9af3a30cb6b0e6d36069267a89747
Кирилл Горюнов <kirillgorunov@MacBook-Pro-Kirill.local>
2024-04-16 22:36:00 +0300

    FIX github search

--- FindRepo/search/github/github.py
+++ FindRepo/search/github/github.py
@@ -7,28 +7,38 @@ import functools
 from typing import List
 
 
-@functools.lru_cache(maxsize=10000)
+forbidden_char = ['"', '@']
+
+
+def filter_code(code: str) -> str:
+    for ch in forbidden_char:
+        code = code.replace(ch, '')
+    return code
+
+
 def find_github(code: str) -> List[str]:
     print('Поиск: Github')
     
-    # config = toml.load('authorization.toml')
-    # token = config['token_github']['token']
+    config = toml.load('authorization.toml')
+    token = config['token_github']['token']
 
-    # headers = {
-    #     'Authorization': token
-    # }
+    headers = {
+        'Authorization': token
+    }
 
-    # url_with_code = f'https://api.github.com/search/code?q={code}'
-    # response = requests.get(url_with_code, headers=headers)
-    # try:
-    #     response_json = response.json()['items']
-    # except:
-    #     return []
+    code_to_request = filter_code(code)
 
-    # answer_list = list()
+    url_with_code = f'https://api.github.com/search/code?q={code_to_request}'
+    response = requests.get(url_with_code, headers=headers)
 
-    # for answer_index in range(len(response_json)):
-    #     answer_list.append(response_json[answer_index]['html_url'])
+    try:
+        response_json = response.json()['items']
+    except Exception as ex:
+        return []
 
-    # return answer_list
-    return []
+    answer_list = list()
+
+    for answer_index in range(len(response_json)):
+        answer_list.append(response_json[answer_index]['html_url'])
+
+    return answer_list
