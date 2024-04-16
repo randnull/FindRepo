b9144ef24594865dfb25702638a863fb37afee50
Кирилл Горюнов <kirillgorunov@MacBook-Pro-Kirill.local>
2024-04-16 22:15:56 +0300

    FIX searcher

--- FindRepo/search/search.py
+++ FindRepo/search/search.py
@@ -1,30 +1,47 @@
+from typing import List
+
 from search.github.github import find_github
 from search.google.google import find_google
 from search.yandex.yandex import find_yandex
 
 
 class Searcher:
-    def __init__(self):
-        pass
+    def __init__(self, is_code):
+        #self.search_functions: List = [('google', find_google)]
+        self.search_functions = []
 
-    def _check_hash(self, hash: str):
-        pass
-
-    def _find_serp(self, object: str):
-        pass
-        # search_functions = [('google', find_google)]
-
-        # if is_code:
-        #     search_functions.append(('github', find_github))
-
-        # answer_list = list()
-
-        # for function in search_functions:
-        #     results = function[1](text)
-        #     answer_list += results
+        if is_code:
+            self.search_functions.append(('github', find_github))
 
 
-        # return list(set(answer_list))
+    def _check_hash(self, hash: str) -> (List, bool): #в БД реп
+        return [], False
 
-    def find(self, text: str, is_code=True):
-        pass
+
+    def _find_serp(self, find_body: str) -> List:
+        links = set()
+        
+        for function in self.search_functions:
+            results = function[1](find_body)
+            
+            for link in links:
+                links.add(link)
+
+        return list(links)
+
+    def find(self, find_object: str):
+        body: str = find_object[0]
+        body_hash: str = find_object[1]
+
+        db_result, is_find = self._check_hash(body_hash)
+
+        if is_find:
+            return db_result
+        
+        links: List = self._find_serp(body)
+
+        print(links)
+
+        #ADD TO DB (body_hash, links)
+
+        return links
