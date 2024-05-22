ad2855a90e150ee8169e717fa9b0d5420ec6e8d3
W1ll1am32 <145133112+W1ll1am32@users.noreply.github.com>
2024-05-22 20:13:09 +0300

    Fixed typos

--- FindRepo/local_search/local_finder.py
+++ FindRepo/local_search/local_finder.py
@@ -7,6 +7,8 @@ from colorama import Fore, Style
 from typing import List, Dict
 
 from common.errors.errors import *
+
+from local_search.database_requests.requests import add_local
 from common.formatter.formatter import FormatterPerHash
 
 from local_search.token_split.token_split import TokenSplit
@@ -15,7 +17,7 @@ from local_search.search.search import LocalSearch
 from common.reader.reader import Reader
 
 
-def local_finder(path: str) -> Dict:
+def local_finder(path: str):
     '''Поиск по базе данных'''
 
     reader: Reader = Reader()
@@ -24,7 +26,7 @@ def local_finder(path: str) -> Dict:
         files: List = reader.read(path)
     except Exception as ErrorBadPath:
         print(Fore.RED + f'{path} не является путем до файла или директории' + Style.RESET_ALL)
-        return dict(), False
+        return dict(), False, dict()
 
     split_class: TokenSplit = TokenSplit(hash_func='md5')
 
@@ -33,17 +35,35 @@ def local_finder(path: str) -> Dict:
     formatter = FormatterPerHash()
 
     links_dict: Dict = dict()
+    count_dict: Dict = dict()
 
     for file, _, file_path in tqdm(files, desc='Поиск совпадений'):
         try:
             file = formatter(file)
-            splitted_current_code = list(split_class.split(file))
+            splitted_current_code: List = list(split_class.split(file))
         except ErrorNotTokenize:
             continue
 
         find_links: List = search_class.find(splitted_current_code, file_path)
 
         for link in find_links:
-            links_dict[link] = links_dict.get(link, 0) + 1
+            links_dict[link] = links_dict.get(link, 0) + find_links[link]
+            count_dict[link] = count_dict.get(link, 0) + 1
 
-    return dict(sorted(links_dict.items(), key=lambda item: -item[1])), True
+        for link in links_dict:
+            links_dict[link] /= count_dict[link]
+
+        value_to_save = search_class.get_value_to_save()
+
+        return dict(sorted(links_dict.items(), key=lambda item: -item[1])), True, value_to_save
+
+
+def save_results(path: str, source: str, value_to_save: List) -> None:
+    if value_to_save is None:
+        return
+
+    if source is None:
+        source = path
+
+    for value_to_save in value_to_save:
+        add_local(value_to_save, source)
