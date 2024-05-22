8466bcb0072692e828c080c88102f57da05bc52f
W1ll1am32 <145133112+W1ll1am32@users.noreply.github.com>
2024-05-22 20:00:33 +0300

    Fixed Formatter

--- FindRepo/common/formatter/formatter.py
+++ FindRepo/common/formatter/formatter.py
@@ -1,4 +1,5 @@
 from typing import List
+import ast
 
 
 class FormatterPerRequest:
@@ -9,10 +10,7 @@ class FormatterPerRequest:
     @staticmethod
     def delete_forbidden_char(raw_object: str) -> str:
         '''Удаляет запрещенные для запроса символы'''
-        github_forbidden_char: List = [".", ",", ":", ";", "/", "\\", "'",
-                                    '"', "=", "*", "!", "?", "#", "$", "&"
-                                    "+", "^", "|", "~", "<", ">", "(", ")",
-                                    "{", "}", "[", "]", "@", '`']
+        github_forbidden_char: List = ['"', '@']
 
         for ch in github_forbidden_char:
             raw_object = raw_object.replace(ch, '')
@@ -41,8 +39,136 @@ class FormatterPerRequest:
         return new_object
 
 
-class FormatterPerHash:
+class FormatterPerHash(ast.NodeTransformer):
     def __init__(self):
-        pass
+        self._in_func = 0
+        self._func_counter = 1
+        self._func_names = {}
+        self._import_names = []
+        self._global_var_counter = 1
+        self._global_var_names = {}
+        self._func_var_counter = 1
+        self._func_var_names = {}
+        self._cur_func_globals = []
 
-    #ast
+    def format(self, code):
+        tree = ast.parse(code)
+        formated_tree = self.visit(tree)
+        self.fix_imports(formated_tree)
+        return ast.unparse(formated_tree)
+
+    def visit_Import(self, node):
+        for n in node.names:
+            self._import_names.append(n.name)
+        return None
+
+    def visit_Name(self, node):
+        if type(node.ctx) is ast.Store:
+            if self._in_func and node.id not in self._cur_func_globals:
+                if node.id in self._func_var_names:
+                    node.id = self._func_var_names[node.id]
+                else:
+                    self._func_var_names[node.id] = 'funcvar' + str(self._func_var_counter)
+                    self._func_var_counter += 1
+                    node.id = self._func_var_names[node.id]
+            else:
+                if node.id in self._global_var_names:
+                    node.id = self._global_var_names[node.id]
+                else:
+                    self._global_var_names[node.id] = 'var' + str(self._global_var_counter)
+                    self._global_var_counter += 1
+                    node.id = self._global_var_names[node.id]
+            return node
+
+        if node.id in self._func_names:
+            node.id = self._func_names[node.id]
+        elif self._in_func and node.id not in self._cur_func_globals:
+            if node.id in self._func_var_names:
+                node.id = self._func_var_names[node.id]
+        else:
+            if node.id in self._global_var_names:
+                node.id = self._global_var_names[node.id]
+        return node
+
+    def visit_AnnAssign(self, node):
+        if isinstance(node.target, ast.AST):
+            self.visit(node.target)
+        new_node = ast.Assign(targets=[node.target], value=node.value)
+        return ast.copy_location(new_node, node)
+
+    def visit_Expr(self, node):
+        if type(node.value) is ast.Constant and type(node.value.value) is str:
+            return None
+        elif type(node.value) is ast.Call:
+            self.visit(node.value)
+        return node
+
+    def visit_BinOp(self, node):
+        if type(node.left) is ast.Name:
+            self.visit(node.left)
+
+        if type(node.right) is ast.Name:
+            self.visit(node.right)
+
+        if type(node.left) is ast.Constant and type(node.right) is ast.Constant:
+            if type(node.op) is ast.Add:
+                new_value = node.left.value + node.right.value
+            if type(node.op) is ast.Sub:
+                new_value = node.left.value - node.right.value
+            if type(node.op) is ast.Mult:
+                new_value = node.left.value * node.right.value
+            if type(node.op) is ast.Div:
+                if node.right.value != 0:
+                    new_value = node.left.value / node.right.value
+
+            new_node = ast.Constant(value=new_value, kind=None)
+            return ast.copy_location(new_node, node)
+        return node
+
+    def visit_FunctionDef(self, node):
+        self._in_func = 1
+
+        for arg in node.args.args:
+            self._func_var_names[arg.arg] = 'funcvar' + str(self._func_var_counter)
+            self._func_var_counter += 1
+            arg.arg = self._func_var_names[arg.arg]
+
+        new_body = node.body
+        size = 0
+        for n in new_body:
+            size += 1
+            if type(n) is ast.Global:
+                self._cur_func_globals.extend(n.names)
+                for i, name in enumerate(n.names):
+                    n.names[i] = self._global_var_names[name]
+                continue
+            self.visit(n)
+            if type(n) is ast.Return:
+                break
+        node.body = new_body[:size]
+        new_name = 'func' + str(self._func_counter)
+
+        self._func_names[node.name] = new_name
+        node.name = new_name
+        self._func_counter += 1
+
+        self._cur_func_globals = []
+        self._in_func = 0
+        return node
+
+    def visit_Call(self, node):
+        if node.func.id in self._func_names:
+            node.func.id = self._func_names[node.func.id]
+        for arg in node.args:
+            if isinstance(arg, ast.AST):
+                self.visit(arg)
+        return node
+
+    def fix_imports(self, node):
+        if self._import_names:
+            names = []
+            self._import_names.sort()
+            for n in self._import_names:
+                names.append(ast.alias(name=n, asname=None))
+            import_node = ast.Import(names=names)
+            node.body.insert(0, import_node)
--- FindRepo/local_search/local_finder.py
+++ FindRepo/local_search/local_finder.py
@@ -7,8 +7,7 @@ from colorama import Fore, Style
 from typing import List, Dict
 
 from common.errors.errors import *
-
-from local_search.database_requests.requests import add_local
+from common.formatter.formatter import FormatterPerHash
 
 from local_search.token_split.token_split import TokenSplit
 from local_search.search.search import LocalSearch
@@ -16,7 +15,7 @@ from local_search.search.search import LocalSearch
 from common.reader.reader import Reader
 
 
-def local_finder(path: str):
+def local_finder(path: str) -> Dict:
     '''Поиск по базе данных'''
 
     reader: Reader = Reader()
@@ -25,41 +24,26 @@ def local_finder(path: str):
         files: List = reader.read(path)
     except Exception as ErrorBadPath:
         print(Fore.RED + f'{path} не является путем до файла или директории' + Style.RESET_ALL)
-        return dict(), False, dict()
+        return dict(), False
 
     split_class: TokenSplit = TokenSplit(hash_func='md5')
 
     search_class: LocalSearch = LocalSearch()
 
+    formatter = FormatterPerHash()
+
     links_dict: Dict = dict()
-    count_dict: Dict = dict()
 
     for file, _, file_path in tqdm(files, desc='Поиск совпадений'):
         try:
-            splitted_current_code: List = list(split_class.split(file))
+            file = formatter(file)
+            splitted_current_code = list(split_class.split(file))
         except ErrorNotTokenize:
             continue
 
-        find_links: Dict = search_class.find(splitted_current_code, file_path)
+        find_links: List = search_class.find(splitted_current_code, file_path)
 
         for link in find_links:
-            links_dict[link] = links_dict.get(link, 0) + find_links[link]
-            count_dict[link] = count_dict.get(link, 0) + 1
+            links_dict[link] = links_dict.get(link, 0) + 1
 
-    for link in links_dict:
-        links_dict[link] /= count_dict[link]
-
-    value_to_save = search_class.get_value_to_save()
-
-    return dict(sorted(links_dict.items(), key=lambda item: -item[1])), True, value_to_save
-
-
-def save_results(path: str, source: str, value_to_save: List) -> None:
-    if value_to_save is None:
-        return
-
-    if source is None:
-        source = path
-
-    for value_to_save in value_to_save:
-        add_local(value_to_save, source)
+    return dict(sorted(links_dict.items(), key=lambda item: -item[1])), True
